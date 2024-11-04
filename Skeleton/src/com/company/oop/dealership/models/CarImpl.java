09243049f7b8cf80f17bd14dfa46219a32fa181e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-04 23:26:56 +0200

    implementing CarImpl

--- Skeleton/src/com/company/oop/dealership/models/CarImpl.java
+++ Skeleton/src/com/company/oop/dealership/models/CarImpl.java
@@ -1,33 +1,48 @@
 package com.company.oop.dealership.models;
 
+import com.company.oop.dealership.models.contracts.Car;
+import com.company.oop.dealership.models.enums.VehicleType;
+import com.company.oop.dealership.utils.ValidationHelpers;
+
+import java.util.List;
+
 import static java.lang.String.format;
 
-public class CarImpl {
+public class CarImpl extends VehicleBase implements Car {
 
-    public static final int MAKE_NAME_LEN_MIN = 2;
-    public static final int MAKE_NAME_LEN_MAX = 15;
-    private static final String MAKE_NAME_LEN_ERR = format(
-            "Make must be between %s and %s characters long!",
-            MAKE_NAME_LEN_MIN,
-            MAKE_NAME_LEN_MAX);
-    public static final int MODEL_NAME_LEN_MIN = 1;
-    public static final int MODEL_NAME_LEN_MAX = 15;
-    private static final String MODEL_NAME_LEN_ERR = format(
-            "Model must be between %s and %s characters long!",
-            MODEL_NAME_LEN_MIN,
-            MODEL_NAME_LEN_MAX);
-    public static final double PRICE_VAL_MIN = 0;
-    public static final double PRICE_VAL_MAX = 1000000;
-    private static final String PRICE_VAL_ERR = format(
-            "Price must be between %.1f and %.1f!",
-            PRICE_VAL_MIN,
-            PRICE_VAL_MAX);
-    public static final int CAR_SEATS_MIN = 1;
-    public static final int CAR_SEATS_MAX = 10;
+    private static final int CAR_SEATS_MIN = 1;
+    private static final int CAR_SEATS_MAX = 10;
     private static final String CAR_SEATS_ERR = format(
-            "Seats must be between %d and %d!",
-            CAR_SEATS_MIN,
-            CAR_SEATS_MAX);
+            "Seats must be between %d and %d!", CAR_SEATS_MIN, CAR_SEATS_MAX);
 
-    //TODO
+
+    private int seats;
+
+    public CarImpl(String make, String model, double price, int seats) {
+
+        super(make, model, price);
+        setSeats(seats);
+        this.wheels = 4;
+        this.vehicleType = VehicleType.CAR;
+
+    }
+
+
+    private void setSeats(int seats) {
+        ValidationHelpers.validateIntRange(seats, CAR_SEATS_MIN, CAR_SEATS_MAX, CAR_SEATS_ERR);
+        this.seats = seats;
+    }
+
+    @Override
+    public int getSeats() {
+        return seats;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("Seats: %d\n", seats);
+    }
+
+
 }
