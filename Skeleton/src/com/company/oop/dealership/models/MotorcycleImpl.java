fe9fe5914c58993845f5361b001e10e44c9c63d2
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-05 17:59:52 +0200

    implementing MotorcycleImpl

--- Skeleton/src/com/company/oop/dealership/models/MotorcycleImpl.java
+++ Skeleton/src/com/company/oop/dealership/models/MotorcycleImpl.java
@@ -1,33 +1,30 @@
 package com.company.oop.dealership.models;
 
-import static java.lang.String.format;
+import com.company.oop.dealership.models.contracts.Motorcycle;
+import com.company.oop.dealership.models.enums.VehicleType;
+import com.company.oop.dealership.utils.VehicleValidations;
 
-public class MotorcycleImpl {
+public class MotorcycleImpl extends VehicleBase implements Motorcycle {
 
-    public static final int MAKE_NAME_LEN_MIN = 2;
-    public static final int MAKE_NAME_LEN_MAX = 15;
-    private static final String MAKE_NAME_LEN_ERR = format(
-            "Make must be between %s and %s characters long!",
-            MAKE_NAME_LEN_MIN,
-            MAKE_NAME_LEN_MAX);
-    public static final int MODEL_NAME_LEN_MIN = 1;
-    public static final int MODEL_NAME_LEN_MAX = 15;
-    private static final String MODEL_NAME_LEN_ERR = format(
-            "Model must be between %s and %s characters long!",
-            MODEL_NAME_LEN_MIN,
-            MODEL_NAME_LEN_MAX);
-    public static final double PRICE_VAL_MIN = 0;
-    public static final double PRICE_VAL_MAX = 1000000;
-    private static final String PRICE_VAL_ERR = format(
-            "Price must be between %.1f and %.1f!",
-            PRICE_VAL_MIN,
-            PRICE_VAL_MAX);
-    public static final int CATEGORY_LEN_MIN = 3;
-    public static final int CATEGORY_LEN_MAX = 10;
-    private static final String CATEGORY_LEN_ERR = format(
-            "Category must be between %d and %d characters long!",
-            CATEGORY_LEN_MIN,
-            CATEGORY_LEN_MAX);
+    private static final int NUMBER_OF_WHEELS_ON_MOTORCYCLE = 2;
+    private static final VehicleType VEHICLE_TYPE = VehicleType.MOTORCYCLE;
+    private final String category;
 
-    //TODO
+    public MotorcycleImpl(String make, String model, double price, String category) {
+        super(make, model, price, NUMBER_OF_WHEELS_ON_MOTORCYCLE, VEHICLE_TYPE);
+
+        VehicleValidations.validateCategory(category);
+        this.category = category;
+    }
+
+
+    @Override
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("Category: %s\n", category);
+    }
 }
