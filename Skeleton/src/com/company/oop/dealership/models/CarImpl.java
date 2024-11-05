3112f1a149ad9dbb5f3941437a9abd5f5af09667
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-05 17:57:03 +0200

    encapsulation + cleaning up code

--- Skeleton/src/com/company/oop/dealership/models/CarImpl.java
+++ Skeleton/src/com/company/oop/dealership/models/CarImpl.java
@@ -1,48 +1,30 @@
 package com.company.oop.dealership.models;
-
 import com.company.oop.dealership.models.contracts.Car;
 import com.company.oop.dealership.models.enums.VehicleType;
-import com.company.oop.dealership.utils.ValidationHelpers;
+import com.company.oop.dealership.utils.VehicleValidations;
 
-import java.util.List;
-
-import static java.lang.String.format;
 
 public class CarImpl extends VehicleBase implements Car {
 
-    private static final int CAR_SEATS_MIN = 1;
-    private static final int CAR_SEATS_MAX = 10;
-    private static final String CAR_SEATS_ERR = format(
-            "Seats must be between %d and %d!", CAR_SEATS_MIN, CAR_SEATS_MAX);
-
-
-    private int seats;
+    private static final int NUMBER_OF_WHEELS_ON_CAR = 4;
+    private static final VehicleType VEHICLE_TYPE = VehicleType.CAR;
+    private final int seats;
 
     public CarImpl(String make, String model, double price, int seats) {
+        super(make, model, price,NUMBER_OF_WHEELS_ON_CAR,VEHICLE_TYPE);
 
-        super(make, model, price);
-        setSeats(seats);
-        this.wheels = 4;
-        this.vehicleType = VehicleType.CAR;
-
-    }
-
-
-    private void setSeats(int seats) {
-        ValidationHelpers.validateIntRange(seats, CAR_SEATS_MIN, CAR_SEATS_MAX, CAR_SEATS_ERR);
+        VehicleValidations.validateSeats(seats);
         this.seats = seats;
     }
+
 
     @Override
     public int getSeats() {
         return seats;
     }
 
-
     @Override
     public String toString() {
         return super.toString() + String.format("Seats: %d\n", seats);
     }
-
-
 }
