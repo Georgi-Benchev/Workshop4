deec0d85e1418f8491eec52ea07e9e0c1607365c
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-05 17:58:39 +0200

    implementing CommentImpl

--- Skeleton/src/com/company/oop/dealership/models/CommentImpl.java
+++ Skeleton/src/com/company/oop/dealership/models/CommentImpl.java
@@ -1,15 +1,44 @@
 package com.company.oop.dealership.models;
 
-import static java.lang.String.format;
+import com.company.oop.dealership.models.contracts.Comment;
+import com.company.oop.dealership.utils.CommentValidation;
 
-public class CommentImpl {
 
-    public static final int CONTENT_LEN_MIN = 3;
-    public static final int CONTENT_LEN_MAX = 200;
-    private static final String CONTENT_LEN_ERR = format(
-            "Content must be between %d and %d characters long!",
-            CONTENT_LEN_MIN,
-            CONTENT_LEN_MAX);
+public class CommentImpl implements Comment {
 
-    //TODO
+
+    private String comment;
+    private final String author;
+
+    public CommentImpl(String comment, String author) {
+        setComment(comment);
+        this.author = author;
+    }
+
+    private void setComment(String comment) {
+        CommentValidation.validateCommentLength(comment);
+        this.comment = comment;
+    }
+
+
+    @Override
+    public String getContent() {
+        return this.comment;
+    }
+
+    @Override
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public String toString() {
+
+        String output =         "\n----------\n";
+        output += String.format("%s\n" +
+                                "User: %s", getContent(), getAuthor());
+        output +=               "\n----------";
+
+        return output;
+    }
 }
