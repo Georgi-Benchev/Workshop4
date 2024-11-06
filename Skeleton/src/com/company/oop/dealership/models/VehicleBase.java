fd417eea1d1df542e402f4e346b936b7dc515cfc
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-06 09:50:42 +0200

    fixing bugs with addComment and removeComment methods and changing the constructor

--- Skeleton/src/com/company/oop/dealership/models/VehicleBase.java
+++ Skeleton/src/com/company/oop/dealership/models/VehicleBase.java
@@ -4,6 +4,7 @@ import com.company.oop.dealership.models.contracts.Comment;
 import com.company.oop.dealership.models.contracts.Commentable;
 import com.company.oop.dealership.models.contracts.Vehicle;
 import com.company.oop.dealership.models.enums.VehicleType;
+import com.company.oop.dealership.utils.FormattingHelpers;
 import com.company.oop.dealership.utils.VehicleValidations;
 
 import java.util.ArrayList;
@@ -11,51 +12,78 @@ import java.util.List;
 
 abstract class VehicleBase implements Vehicle, Commentable {
 
+    private final String make;
+    private final String model;
+    private final double price;
+    private final int wheels;
+    private final VehicleType vehicleType;
+    private final List<Comment> comments;
 
-    private String make;
-    private String model;
-    private double price;
-    protected int wheels;
-    protected VehicleType vehicleType;
-    private List<Comment> comments;
 
-    protected VehicleBase(String make, String model, double price) {
-        setMake(make);
-        setModel(model);
-        setPrice(price);
+    protected VehicleBase(String make, String model, double price, int wheels, VehicleType vehicleType) {
+        VehicleValidations.validatePrice(price);
+        VehicleValidations.validateMake(make);
+        VehicleValidations.validateModel(model);
+        this.model = model;
+        this.make = make;
+        this.price = price;
+        this.wheels = wheels;
+        this.vehicleType = vehicleType;
         comments = new ArrayList<>();
     }
 
-    private void setMake(String make) {
-        VehicleValidations.validateMake(make);
-        this.make = make;
+
+    @Override
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
     }
 
-    public void setModel(String model) {
-       VehicleValidations.validateModel(model);
-        this.model = model;
+    @Override
+    public void removeComment(Comment comment) {
+        this.comments.remove(comment);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(". %s:\n" +
+                        "Make: %s\n" +
+                        "Model: %s\n" +
+                        "Wheels: %d\n" +
+                        "Price: $%s\n",
+                vehicleType, make, model, wheels, FormattingHelpers.removeTrailingZerosFromDouble(price));
+    }
+
+    public String printComments() {
+        StringBuilder output = new StringBuilder();
+        output.append("--COMMENTS--");
+
+        for (Comment comment : comments) {
+            output.append(comment.toString());
+        }
+
+        output.append("\n");
+        output.append("--COMMENTS--");
+
+        return output.toString();
     }
 
 
+    //  GETTERS
 
-    public void setPrice(double price) {
-       VehicleValidations.validatePrice(price);
-        this.price = price;
+    @Override
+    public List<Comment> getComments() {
+        return new ArrayList<>(comments);
     }
-
-
 
     @Override
     public int getWheels() {
         return wheels;
-
     }
 
     @Override
     public VehicleType getType() {
         return vehicleType;
     }
-
 
     @Override
     public String getMake() {
@@ -71,31 +99,4 @@ abstract class VehicleBase implements Vehicle, Commentable {
     public String getModel() {
         return this.model;
     }
-
-    @Override
-    public void addComment(Comment comment) {
-       this.comments.add(comment);
-    }
-
-    @Override
-    public void removeComment(Comment comment) {
-        this.comments.remove(comment);
-    }
-
-    @Override
-    public String toString() {
-        String output = String.format("#. %s:\n" +
-                "Make: %s\n" +
-                "Model: %s\n" +
-                "Wheels: %d\n" +
-                "Price: $%.2f\n", vehicleType, make, model, wheels, price);
-        return output;
-    }
-
-    @Override
-    public List<Comment> getComments() {
-        return new ArrayList<>(comments);
-    }
-
-
 }
