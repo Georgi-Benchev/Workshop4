aa634d65f9ebf2427c1e4bae7ee6e217068aaad4
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-06 09:47:02 +0200

    better implementation for TruckImpl class

--- Skeleton/src/com/company/oop/dealership/models/TruckImpl.java
+++ Skeleton/src/com/company/oop/dealership/models/TruckImpl.java
@@ -1,33 +1,31 @@
 package com.company.oop.dealership.models;
 
-import static java.lang.String.format;
+import com.company.oop.dealership.models.contracts.Truck;
+import com.company.oop.dealership.models.enums.VehicleType;
+import com.company.oop.dealership.utils.VehicleValidations;
 
-public class TruckImpl {
 
-    public static final int MAKE_NAME_LEN_MIN = 2;
-    public static final int MAKE_NAME_LEN_MAX = 15;
-    private static final String MAKE_NAME_LEN_ERR = format(
-            "Make must be between %s and %s characters long!",
-            MAKE_NAME_LEN_MIN,
-            MAKE_NAME_LEN_MAX);
-    public static final int MODEL_NAME_LEN_MIN = 1;
-    public static final int MODEL_NAME_LEN_MAX = 15;
-    private static final String MODEL_NAME_LEN_ERR = format(
-            "Model must be between %s and %s characters long!",
-            MODEL_NAME_LEN_MIN,
-            MODEL_NAME_LEN_MAX);
-    public static final double PRICE_VAL_MIN = 0;
-    public static final double PRICE_VAL_MAX = 1000000;
-    private static final String PRICE_VAL_ERR = format(
-            "Price must be between %.1f and %.1f!",
-            PRICE_VAL_MIN,
-            PRICE_VAL_MAX);
-    public static final int WEIGHT_CAP_MIN = 1;
-    public static final int WEIGHT_CAP_MAX = 100;
-    private static final String WEIGHT_CAP_ERR = format(
-            "Weight capacity must be between %d and %d!",
-            WEIGHT_CAP_MIN,
-            WEIGHT_CAP_MAX);
+public class TruckImpl extends VehicleBase implements Truck {
 
-    //TODO
+    private static final int NUMBER_OF_WHEELS_ON_TRUCK = 8;
+    private static final VehicleType VEHICLE_TYPE = VehicleType.TRUCK;
+    private final int weightCap;
+
+    public TruckImpl(String make, String model, double price, int weightCap) {
+        super(make, model, price, NUMBER_OF_WHEELS_ON_TRUCK, VEHICLE_TYPE);
+
+        VehicleValidations.validateWeightCapacity(weightCap);
+        this.weightCap = weightCap;
+    }
+
+
+    @Override
+    public int getWeightCapacity() {
+        return weightCap;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("Weight Capacity: %dt\n", getWeightCapacity());
+    }
 }
